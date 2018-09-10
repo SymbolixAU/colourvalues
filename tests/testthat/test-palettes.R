9623bfc837cc547eef83eee8ef271edd298c404f
SymbolixAU <dcooley@symbolix.com.au>
2018-09-10 12:55:27 +1000

    tests

---
+++ tests/testthat/test-palettes.R
@@ -0,0 +1,16 @@
+context("palettes")
+
+test_that("palettes are returned", {
+
+  dfv <- viridis()
+  dfp <- plasma()
+  dfi <- inferno()
+  dfm <- magma()
+  dfc <- cividis()
+  expect_true(nrow(dfv) == 256 & ncol(dfv) == 3)
+  expect_true(nrow(dfp) == 256 & ncol(dfp) == 3)
+  expect_true(nrow(dfi) == 256 & ncol(dfi) == 3)
+  expect_true(nrow(dfm) == 256 & ncol(dfm) == 3)
+  expect_true(nrow(dfc) == 256 & ncol(dfc) == 3)
+
+})
