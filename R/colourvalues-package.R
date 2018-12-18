f794e55a5cc28a5646430319d80168ea2838a365
SymbolixAU <dcooley@symbolix.com.au>
2018-12-18 19:36:34 +1100

    fix tests

--- R/colourvalues-package.R
+++ R/colourvalues-package.R
@@ -14,3 +14,10 @@ palette_check.character <- function( palette ) tolower( gsub("\\.","_", palette 
 
 #' @export
 palette_check.default <- function( palette ) palette
+
+#' @export
+palette_check.matrix <- function( palette ) {
+  if ( nrow( palette ) < 5 )
+    stop("A matrix palette must contain at least 5 rows")
+  return( palette )
+}
