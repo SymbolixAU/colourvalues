530a73a245595bc7583e0707f3801ae127ebcb5d
SymbolixAU <dcooley@symbolix.com.au>
2019-01-14 08:05:03 +1100

    tests to close #37

---
+++ tests/testthat/test-alpha.R
@@ -0,0 +1,17 @@
+context("alpha")
+
+
+test_that("[0,1) alpha values treated as percentage", {
+
+  expect_true( colour_values(1, alpha = 0.5) == colour_values(1, alpha = 255 / 2) )
+  expect_true( colour_values(1, alpha = 1) == colour_values(1, alpha = 255 / 255 ) )
+
+})
+
+test_that("alpha values > 255 are scaled", {
+
+  expect_true( colour_values(1, alpha = 255 ) == colour_values(1, alpha = 255 * 2 + 1 ) )
+  expect_true( colour_values(1, alpha = 255 ) == colour_values(1, alpha = 255 * 3 + 2 ) )
+  expect_true( colour_values(1, alpha = 255 ) == colour_values(1, alpha = 255 * 10 + 9 ) )
+
+})
