c447faf3d0eb5aa70433e352ef07d1451d9fb7c9
David <david.cooley141@gmail.com>
2020-08-17 08:30:39 +1000

    removed scratch

--- R/scratch.R
+++ R/scratch.R
@@ -1,10 +1,10 @@
 
-x <- 1:10
-pal <- "viridis"
-alpha <- 255
-repeats <- c(2,2,2,2,5,3,2,1,1,10)
-total <- sum( repeats )
-
-colourvalues:::rcpp_colour_values_rgb_interleaved(
-  x, pal, alpha, repeats, total, n_summaries = 5
-)
+# x <- 1:10
+# pal <- "viridis"
+# alpha <- 255
+# repeats <- c(2,2,2,2,5,3,2,1,1,10)
+# total <- sum( repeats )
+#
+# colourvalues:::rcpp_colour_values_rgb_interleaved(
+#   x, pal, alpha, repeats, total, n_summaries = 5
+# )
